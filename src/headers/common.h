f7209a2e8211d6bec4b94179fafe0d4fea6fdb39
Etistab <baptiste.lego1@gmail.com>
2018-11-22 12:41:39 +0100

    Clean src

--- src/headers/common.h
+++
@@ -1,9 +0,0 @@
-#include "struct.h"
-
-void displayHomeScreen(SDL_Surface* screen, Config *config, Colors *colors);
-void mainEventLoop(SDL_Surface* screen, Config *config);
-void createRect(SDL_Surface *screen, int width, int height, int x, int y, Uint32 color);
-void verifyPointer(void *pointer, char* message);
-void setPosition(SDL_Rect *position, int x, int y);
-void loadColors(SDL_Surface *screen, Colors *colors);
-void loadConfig(Config *config);
