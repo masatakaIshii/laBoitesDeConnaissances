3ea3c2ef14101aa80b74abddcbef15ab191600e3
Etistab <baptiste.lego1@gmail.com>
2018-11-22 10:37:07 +0100

    début du projet --> on pose les bases

---
+++ src/headers/struct.h
@@ -0,0 +1,14 @@
+#include <SDL.h>
+#define BPP     32
+
+typedef struct Colors{
+    Uint32 blue;
+    Uint32 lightblue;
+    Uint32 green;
+    Uint32 yellow;
+}Colors;
+
+typedef struct Config{
+    int width;
+    int height;
+}Config;
